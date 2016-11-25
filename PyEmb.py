ed20e64133156f7a185e5ea7fd7c9ffee16d1f7c
Serg Stetsuk <serg_stetsuk@ukr.net>
2016-11-25 17:04:18 +0200

    dbg for Windows users. Cache disabled

--- PyEmb.py
+++ PyEmb.py
@@ -5,14 +5,14 @@ import math
 import sys
 from copy import deepcopy
 
-try:
-    from functools import lru_cache
-except ImportError:
-    from backports.functools_lru_cache import lru_cache
+#try:
+#    from functools import lru_cache
+#except ImportError:
+#    from backports.functools_lru_cache import lru_cache
 
 # simplify use of lru_cache decorator
-def cache(*args, **kwargs):
-    return lru_cache(maxsize=None)(*args, **kwargs)
+#def cache(*args, **kwargs):
+#    return lru_cache(maxsize=None)(*args, **kwargs)
 
 class Point:
 
