d433c032bc490764f6d2a61b03648b5b5bcd62f1
Lex Neva <github.com@lexneva.name>
2018-02-26 19:33:05 -0500

    embroidery origin defaults to center of canvas

--- inkstitch.py
+++ inkstitch.py
@@ -526,15 +526,19 @@ def get_origin(svg):
     guides = [guide for guide in all_guides
                     if guide.get(label_attribute, "").startswith("embroidery origin")]
 
-    if len(guides) < 2:
-        return Point(0, 0)
-
     # document size used below
     doc_size = list(get_doc_size(svg))
 
     # convert the size from viewbox-relative to real-world pixels
     viewbox_transform = get_viewbox_transform(svg)
     simpletransform.applyTransformToPoint(simpletransform.invertTransform(viewbox_transform), doc_size)
+
+    default = [doc_size[0] / 2.0, doc_size[1] / 2.0]
+    simpletransform.applyTransformToPoint(viewbox_transform, default)
+    default = Point(*default)
+
+    if len(guides) < 2:
+        return default
 
     # Find out where the guides intersect.  Only pay attention to the first two.
     guides = guides[:2]
@@ -561,11 +565,11 @@ def get_origin(svg):
 
     if isinstance(intersection, shgeo.Point):
         origin = [intersection.x, intersection.y]
-        simpletransform.applyTransformToPoint(get_viewbox_transform(svg), origin)
+        simpletransform.applyTransformToPoint(viewbox_transform, origin)
         return Point(*origin)
     else:
         # Either the two guides are the same line, or they're parallel.
-        return Point(0, 0)
+        return default
 
 
 def write_embroidery_file(file_path, stitches, svg):
