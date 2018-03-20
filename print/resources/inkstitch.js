fe481cbb7fa7f7e3c81d76f29ab8a8bd32e13697
Kaalleen <36401965+kaalleen@users.noreply.github.com>
2018-03-20 20:41:10 +0100

    Update inkstitch.js
    shutdown the server when closing the browser

--- print/resources/inkstitch.js
+++ print/resources/inkstitch.js
@@ -1,4 +1,10 @@
 $(function() {
+  
+  // If the window is closed, we still want to be sure to shutdown the server
+  window.addEventListener('beforeunload', setTimeout(function() { 
+    $.post('/shutdown', {})
+  }), 1000);
+  
   $('button.close').click(function() {
     $.post('/shutdown', {})
      .done(function(data) {
