60a4a05289ac811439e8bf02c72493883822684a
kalleen <reni@allenka.de>
2018-03-22 21:51:33 +0100

    proposal on how to handle contenteditable fields

--- print/resources/inkstitch.js
+++ print/resources/inkstitch.js
@@ -20,9 +20,19 @@ $(function() {
     $('#settings-ui').show();
   });
 
-  $("#close-settings").click(function(){
-      $("#settings-ui").hide();
-  }); 
+  $('#close-settings').click(function(){
+      $('#settings-ui').hide();
+  });
+  
+  /* When we focus out from a contenteditable field, we want to
+     set the same content to all fields with the same classname */
+  document.querySelectorAll('[contenteditable="true"]').forEach(function(elem) {
+    elem.addEventListener('focusout', function() {
+        var content = $(this).html();
+        var style = $(this).attr('class');
+        $('.' + style).html(content);
+    });
+  });
   
 });
 
