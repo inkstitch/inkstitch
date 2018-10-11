41e957a0c2c0479f2d01e6551cdf1502f825fd52
Lex Neva <github.com@lexneva.name>
2018-10-10 21:21:11 -0400

    add guide parsing library

---
+++ lib/svg/guides.py
@@ -0,0 +1,39 @@
+from .tags import SODIPODI_NAMEDVIEW, SODIPODI_GUIDE, INKSCAPE_LABEL
+from .units import get_doc_size, get_viewbox_transform
+from ..utils import string_to_floats, Point, cache
+
+class InkscapeGuide(object):
+    def __init__(self, node):
+        self.node = node
+        self.svg = node.getroottree().getroot()
+
+        self._parse()
+
+    def _parse(self):
+        self.label = self.node.get(INKSCAPE_LABEL, "")
+
+        doc_size = list(get_doc_size(self.svg))
+
+        # convert the size from viewbox-relative to real-world pixels
+        viewbox_transform = get_viewbox_transform(svg)
+        simpletransform.applyTransformToPoint(simpletransform.invertTransform(viewbox_transform), doc_size)
+
+        self.position = Point(*string_to_floats(self.node.get('position')))
+
+        # inkscape's Y axis is reversed from SVG's, and the guide is in inkscape coordinates
+        self.position.y = doc_size[1] - self.position.y
+
+        # This one baffles me.  I think inkscape might have gotten the order of
+        # their vector wrong?
+        parts = string_to_floats(self.node.get('orientation'))
+        self.direction = Point(parts[1], parts[0])
+
+@cache
+def get_guides(svg):
+    """Find all Inkscape guides and return as InkscapeGuide instances."""
+
+    namedview = svg.find(SODIPODI_NAMEDVIEW)
+    if namedview is None:
+        return []
+
+    return [InkscapeGuide(node) for node in namedview.findall(SODIPODI_GUIDE)]
--- lib/utils/__init__.py
+++ lib/utils/__init__.py
@@ -3,3 +3,4 @@ from cache import cache
 from io import *
 from inkscape import *
 from paths import *
+from string import *
