5de9f06d36cce92cf384be90ce95f2a1938e1a58
Lex Neva <github.com@lexneva.name>
2018-10-20 20:01:25 -0400

    fix style

--- inkstitch.py
+++ inkstitch.py
@@ -1,9 +1,10 @@
-import sys
 import os
+import sys
 import traceback
 from argparse import ArgumentParser
-from lib.utils import save_stderr, restore_stderr
+
 from lib import extensions
+from lib.utils import save_stderr, restore_stderr
 
 
 parser = ArgumentParser()
@@ -18,8 +19,9 @@ if os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), "DEB
     #    * follow the "Note:" to enable the debug server menu item
     # 3. Create a file named "DEBUG" next to inkstitch.py in your git clone.
     # 4. Run any extension and PyDev will start debugging.
-    
-    import pydevd; pydevd.settrace()
+
+    import pydevd
+    pydevd.settrace()
 
 extension_name = my_args.extension
 
