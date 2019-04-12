5b5cde330fd21289fcdce7f43d37c54b7cc73310
Lex Neva <github.com@lexneva.name>
2019-04-12 16:48:25 -0400

    fix dev path

--- lib/gui/electron.py
+++ lib/gui/electron.py
@@ -17,9 +17,10 @@ def open_url(url):
     else:
         # It's a bit trickier to find the electron app in a development environment.
         base_dir = get_bundled_dir("electron")
-        package_dir = glob(os.path.join(base_dir, 'dist', '*-unpacked'))
 
-        if not package_dir:
+        try:
+            package_dir = glob(os.path.join(base_dir, 'dist', '*-unpacked'))[0]
+        except IndexError:
             raise Exception("Electron app not found.  Be sure to run 'yarn; yarn dist' in %s." % base_dir)
 
         electron_path = os.path.join(base_dir, package_dir, "inkstitch-gui")
