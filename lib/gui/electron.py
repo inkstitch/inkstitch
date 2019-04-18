67027f07046aecc9e45785e077ad7ee70b365c0c
Kate Murphy <hello@kate.io>
2019-04-17 21:48:44 -0400

    Fix crashed caused by Inkscape reading message from stdout

--- lib/gui/electron.py
+++ lib/gui/electron.py
@@ -11,16 +11,19 @@ app_process = None
 def open_url(url):
     global app
 
+    # Any output on stdout will crash inkscape.
+    null = open(os.devnull, 'w')
+
     if getattr(sys, 'frozen', None) is not None:
         electron_path = os.path.join(get_bundled_dir("electron"), "inkstitch-gui")
 
         if sys.platform == "darwin":
             electron_path += ".app/Contents/MacOS/inkstitch-gui"
-            subprocess.Popen(["open", "-a", electron_path, "--args", url])
+            subprocess.Popen(["open", "-a", electron_path, "--args", url], stdout=null)
         else:
             app_process = subprocess.Popen([electron_path, url])
     else:
         # if we're not running in a pyinstaller bundle, run electron directly
-        app_process = subprocess.Popen(["yarn", "dev", url], cwd=get_bundled_dir("electron"))
+        app_process = subprocess.Popen(["yarn", "dev", url], cwd=get_bundled_dir("electron"), stdout=null)
 
     return app_process
