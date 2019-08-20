ba531add9fde0d6c43d992d9bef21a4a834050f4
Lex Neva <github.com@lexneva.name>
2019-08-20 10:04:20 -0400

    cleanup

--- electron/src/simulator.js
+++ electron/src/simulator.js
@@ -1,40 +1,10 @@
 const inkStitch = require("./lib/api")
 const SVG = require("svg.js")
-//inkStitch.get('ping').then(response => { document.querySelector("#content").innerHTML = response.data })
 
 var fps = document.getElementById("fps")
-//var frame = 0
 
 var container = document.getElementById("simulation")
-var simulation = SVG("simulation").size(800, 800)
-//const black_stroke = {fill: "none", stroke: "black"}
-////var x = 0
-////var y = 10
-//
-//var paths = Array()
-//
-//for (let y = 0; y < 800; y += 10) {
-//	for (let x = 0; x < 800; x += 1) {
-//		paths.push(simulation.path(`M${x},${y} ${x},${y + 10}`).attr(black_stroke).hide());
-//	}
-//}
-//
-//var i = 0;
-//
-//setInterval(function() {
-//	let now = performance.now()
-//	let frame_time = (now - last_tick) / 1000.0;
-//	last_tick = now
-//
-//	let segments = 300 * frame_time;
-//	
-//	fps.innerHTML = `${segments} ${frame_time}`
-//	
-//	for (let j=0; j < segments; j++) {
-//    	    paths[i].show();
-//	    i++;
-//	}
-//}, 33);
+var simulation = SVG("simulation").size("90%", "85%")
 
 var stitches = Array()
 
@@ -54,7 +24,7 @@ inkStitch.get('simulator/get_stitch_plan').then(response => {
 			prevStitch = stitch
 		})
 	})
-	console.log(performance.now() - starts)
+	console.log(performance.now() - start)
 
 	container.style.display = "";
 
