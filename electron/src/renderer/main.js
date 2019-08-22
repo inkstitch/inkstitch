1cccb7beabf885715ae6af15d723c21786605897
Lex Neva <github.com@lexneva.name>
2019-08-21 21:26:52 -0400

    vue-ify using simulatedgreg/electron-vue my-project

---
+++ electron/src/renderer/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  template: '<App/>'
+}).$mount('#app')
