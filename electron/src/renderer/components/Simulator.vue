aaa89ffec4f2a1cbd28bc8cdeff2a96a8dcc7c7a
Lex Neva <github.com@lexneva.name>
2019-08-23 16:07:59 -0400

    add keyboard controls and speed/stitch/command display

--- electron/src/renderer/components/Simulator.vue
+++ electron/src/renderer/components/Simulator.vue
@@ -1,84 +1,221 @@
 <template>
   <div>
+    <div class="simulator"></div>
+    <div class="display">
+      speed: {{speedDisplay}} stitches/sec   current stitch: {{currentStitchDisplay}}/{{numStitches}}  animating: {{animating}}  command: {{currentCommand}}
+    </div>
   </div>
 </template>
 
 <script>
-  const SVG = require("svg.js")
   const inkStitch = require("../../lib/api")
+  const Mousetrap = require("mousetrap")
+  const SVG = require("svg.js")
   require("svg.panzoom.js")
 
   export default {
-  	name: 'simulator',
-	  mounted: function() {
-      var svg = SVG(this.$el).size("90%", "85%").panZoom({zoomMin: 0.1})
-      var simulation = svg.group()
-      var stitches = Array()
+    name: 'simulator',
+    data: function() {
+      return {
+        speed: 16,
+        currentStitch: 1,
+        direction: 1,
+        numStitches: 0,
+        animating: false
+      }
+    },
+    computed: {
+      currentStitchDisplay() {
+        return this.currentStitch.toFixed()
+      },
+      speedDisplay() {
+        return this.speed * this.direction
+      },
+      currentCommand() {
+        let stitch = this.stitches[this.currentStitch.toFixed()]
 
-      var speed = 16
-      const target_fps = 30
-      const target_frame_period = 1000.0 / target_fps
+        if (stitch === undefined) {
+          return ""
+        }
+
+        let label = "STITCH"
+        switch(true) {
+          case stitch.jump:
+            label = "JUMP"
+            break
+          case stitch.trim:
+            label = "TRIM"
+            break
+          case stitch.stop:
+            label = "STOP"
+            break
+          case stitch.color_change:
+            label = "COLOR CHANGE"
+            break
+        }
+
+        return label
+      }
+    },
+    methods: {
+      animationSpeedUp() {
+        this.speed *= 2.0
+      },
+      animationSlowDown() {
+        this.speed = Math.max(this.speed/2.0, 1)
+      },
+      animationReverse() {
+        this.direction = -1
+        this.start()
+      },
+      animationForward() {
+        this.direction = 1
+        this.start()
+      },
+      toggleAnimation() {
+        if (this.animating) {
+          this.stop()
+        } else {
+          this.start()
+        }
+      },
+      animationForwardOneFrame() {
+        this.stop()
+        this.currentStitch++
+        this.clampCurrentStitch()
+        this.renderFrame()
+      },
+      animationBackwardOneFrame() {
+        this.stop()
+        this.currentStitch--
+        this.clampCurrentStitch()
+        this.renderFrame()
+      },
+      clampCurrentStitch() {
+        this.currentStitch = Math.max(Math.min(this.currentStitch, this.numStitches), 1)
+      },
+      animate() {
+        let frameStart = performance.now()
+        let frameTime = null
+
+        if (this.lastFrameStart !== null) {
+          frameTime = frameStart - this.lastFrameStart
+        } else {
+          frameTime = this.targetFramePeriod
+        }
+
+        this.lastFrameStart = frameStart
+
+        let numStitches = this.speed * Math.max(frameTime, this.targetFramePeriod) / 1000.0;
+        this.currentStitch = this.currentStitch + numStitches * this.direction
+        this.clampCurrentStitch()
+
+        this.renderFrame()
+
+        if (this.animating && this.shouldAnimate()) {
+          this.timer = setTimeout(this.animate, Math.max(0, this.targetFramePeriod - frameTime))
+        } else {
+          this.timer = null;
+          this.stop()
+        }
+      },
+      renderFrame() {
+        while (this.renderedStitch < this.currentStitch) {
+          this.renderedStitch += 1
+          this.stitchPaths[this.renderedStitch].show();
+        }
+
+        while (this.renderedStitch >= this.currentStitch) {
+          this.stitchPaths[this.renderedStitch].hide();
+          this.renderedStitch -= 1
+        }
+      },
+      shouldAnimate() {
+        if (this.direction == 1 && this.currentStitch < this.numStitches) {
+          return true;
+        } else if (this.direction == -1 && this.currentStitch > 1) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      start() {
+        if (!this.animating && this.shouldAnimate()) {
+          this.animating = true
+          this.timer = setTimeout(this.animate, 0);
+        }
+      },
+      stop() {
+        if (this.animating) {
+          if (this.timer) {
+            clearTimeout(this.timer)
+            this.timer = null
+          }
+          this.animating = false
+          this.lastFrameStart = null
+        }
+      }
+    },
+    created: function() {
+      // non-reactive properties
+      this.targetFPS = 30
+      this.targetFramePeriod = 1000.0 / this.targetFPS,
+      this.renderedStitch = 0
+      this.lastFrameStart = null
+      this.stitchPaths = [null]  // 1-indexed to match up with stitch number display
+      this.stitches = [null]
+      this.svg = null
+      this.simulation = null
+      this.timer = null
+    },
+	  mounted: function() {
+      this.svg = SVG(this.$el.querySelector(".simulator")).size("90%", "85%").panZoom({zoomMin: 0.1})
+      this.simulation = this.svg.group()
 
       inkStitch.get('stitch_plan').then(response => {
         var stitch_plan = response.data
           let [minx, miny, maxx, maxy] = stitch_plan.bounding_box    
           let width = maxx - minx
           let height = maxy - miny    
-          svg.viewbox(0, 0, width, height);
+          this.svg.viewbox(0, 0, width, height);
         
-        console.log(stitch_plan)
         stitch_plan.color_blocks.forEach(color_block => {
           let attrs = {fill: "none", stroke: `${color_block.color.visible_on_white.hex}`, "stroke-width": 0.3}
+          let stitching = false
           let prevStitch = null
           color_block.stitches.forEach(stitch => {
             stitch.x -= minx
             stitch.y -= miny
-            if (stitch.trim) {
-              prevStitch = null
-            } else if (stitch.color_change) {
-              // ignore
+
+            if (stitching && prevStitch) {
+              this.stitchPaths.push(this.simulation.path(`M${prevStitch.x},${prevStitch.y} ${stitch.x},${stitch.y}`).attr(attrs).hide())
             } else {
-              if (prevStitch) {
-                stitches.push(simulation.path(`M${prevStitch.x},${prevStitch.y} ${stitch.x},${stitch.y}`).attr(attrs).hide())
-              }
-              prevStitch = stitch
+              this.stitchPaths.push(this.simulation.path(`M${stitch.x},${stitch.y} ${stitch.x},${stitch.y}`).attr(attrs).hide())
             }
+            this.stitches.push(stitch)
+
+            if (stitch.trim || stitch.color_change) {
+              stitching = false
+            } else if (!stitch.jump) {
+              stitching = true
+            }
+
+            prevStitch = stitch
           })
         })
 
-        var last_frame_start = performance.now()
-        var current_stitch = 1
-        var rendered_stitch = 0
-        var direction = 1
+        this.numStitches = this.stitches.length - 1
 
-        var renderFrame = function() {
-          let frame_start = performance.now();
-          let frame_time = frame_start - last_frame_start
-          last_frame_start = frame_start
-          
-          let num_stitches = speed * Math.max(frame_time, target_frame_period) / 1000.0;
-          current_stitch = Math.max(Math.min(current_stitch + num_stitches * direction, stitches.length - 1), 0)
-          
-          //fps.innerHTML = `${num_stitches.toFixed(2)} ${last_frame_time.toFixed(2)}`
-          
-          while (rendered_stitch + 1 <= current_stitch) {
-            rendered_stitch += 1
-            stitches[rendered_stitch].show();
-          }
-          
-          while (rendered_stitch - 1 >= current_stitch) {
-            stitches[rendered_stitch].hide();
-            rendered_stitch -= 1
-          }
-          
-          //fps.innerHTML = `${num_stitches.toFixed(2)} ${frame_time.toFixed(2)}`
-          
-          if (current_stitch < stitches.length - 1) {
-            setTimeout(renderFrame, Math.max(0, target_frame_period - frame_time))
-          }
-        }
-        
-        renderFrame()
+        // v-on:keydown doesn't seem to work, maybe an Electron issue?
+        Mousetrap.bind("up", this.animationSpeedUp)
+        Mousetrap.bind("down", this.animationSlowDown)
+        Mousetrap.bind("left", this.animationReverse)
+        Mousetrap.bind("right", this.animationForward)
+        Mousetrap.bind("space", this.toggleAnimation)
+        Mousetrap.bind("+", this.animationForwardOneFrame)
+        Mousetrap.bind("-", this.animationBackwardOneFrame)
+
+        this.start()
       })
   	}
   }
