5e272029028d534dad041c29ab9da2725cac29f6
Momo <reni@allenka.de>
2019-08-31 19:26:18 +0200

    embroider clones too

--- lib/svg/svg.py
+++ lib/svg/svg.py
@@ -27,5 +27,6 @@ def generate_unique_id(document_or_element, prefix="path"):
 
 
 def find_elements(node, xpath):
-    elements = node.xpath(xpath, namespaces=NSS)
+    document = get_document(node)
+    elements = document.xpath(xpath, namespaces=NSS)
     return elements
