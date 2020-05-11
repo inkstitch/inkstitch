779d0a7c654a763ba1a83804d824240d065612e6
Kaalleen <reni@allenka.de>
2020-05-11 20:42:34 +0200

    fill class independent

--- lib/extensions/break_apart.py
+++ lib/extensions/break_apart.py
@@ -1,41 +1,34 @@
-import logging
-from copy import deepcopy
+from copy import copy
 
 from shapely.geometry import LineString, Polygon
 from shapely.ops import polygonize, unary_union
 
 import inkex
 
-from ..elements import AutoFill, Fill
+from ..elements import EmbroideryElement
 from ..i18n import _
 from ..svg import get_correction_transform
+from ..svg.tags import SVG_PATH_TAG
 from .base import InkstitchExtension
 
 
 class BreakApart(InkstitchExtension):
     '''
-    This will solve crossing border and holes lies outside shell errors for fill shapes
-    and breaks them into multiple elements if necessary.
+    This will break apart fill areas into separate elements.
     '''
     def effect(self):
-        if not self.get_elements():
-            return
-
         if not self.selected:
             inkex.errormsg(_("Please select one or more fill areas to break apart."))
             return
 
-        for element in self.elements:
-            if not isinstance(element, AutoFill) and not isinstance(element, Fill):
-                continue
+        elements = []
+        nodes = self.get_nodes()
+        for node in nodes:
+            if node.tag in SVG_PATH_TAG:
+                elements.append(EmbroideryElement(node))
 
-            # ignore valid elements
-            logger = logging.getLogger('shapely.geos')
-            level = logger.level
-            logger.setLevel(logging.CRITICAL)
-            valid = element.shape.is_valid
-            logger.setLevel(level)
-            if valid:
+        for element in elements:
+            if not element.get_style("fill", "black"):
                 continue
 
             multipolygons = self.break_apart_element(element)
@@ -43,9 +36,10 @@ class BreakApart(InkstitchExtension):
                 self.element_to_nodes(multipolygons, element)
 
     def break_apart_element(self, element):
+        paths = element.flatten(element.parse_path())
         polygons = []
 
-        for path in element.paths:
+        for path in paths:
             linestring = LineString(path)
             if not linestring.is_simple:
                 linestring = unary_union(linestring)
@@ -80,21 +74,29 @@ class BreakApart(InkstitchExtension):
         return multipolygons
 
     def element_to_nodes(self, multipolygons, element):
+        index = element.node.getparent().index(element.node)
+
         for polygons in multipolygons:
             # ignore very small areas
             if polygons[0].area < 5:
                 continue
-            el = deepcopy(element)
+
+            el = copy(element.node)
             d = ""
+
             for polygon in polygons:
+                if len(multipolygons) == 1:
+                    node_id = el.get('id')
+                else:
+                    node_id = self.uniqueId(el.get('id') + '_')
                 # copy element and replace path
-                el.node.set('id', self.uniqueId(element.node.get('id') + '_'))
+                el.set('id', node_id)
                 d += "M"
                 for x, y in polygon.exterior.coords:
                     d += "%s,%s " % (x, y)
                     d += " "
                 d += "Z"
-            el.node.set('d', d)
-            el.node.set('transform', get_correction_transform(element.node))
-            element.node.getparent().insert(0, el.node)
+            el.set('d', d)
+            el.set('transform', get_correction_transform(element.node))
+            element.node.getparent().insert(index, el)
         element.node.getparent().remove(element.node)
