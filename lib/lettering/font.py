88a0618523202df31ef9e2aafda4918522155fe8
LaureFR38 <63351363+LaureFR38@users.noreply.github.com>
2020-10-09 23:57:44 +0200

    Update font.py

--- lib/lettering/font.py
+++ lib/lettering/font.py
@@ -210,11 +210,11 @@ class Font(object):
         node = deepcopy(glyph.node)
 
         if last_character is not None:
-            position.x += self.letter_spacing + self.kerning_pairs_px.get(last_character + character, self.kerning_pairs.get(last_character + character, 0) * PIXELS_PER_MM)    
+            position.x -= -self.letter_spacing + self.kerning_pairs_px.get(last_character + character, -self.kerning_pairs.get(last_character + character, 0) * PIXELS_PER_MM)    
 
         transform = "translate(%s, %s)" % position.as_tuple()
         node.set('transform', transform)
-        position.x -= self.horiz_adv_x.get(character, -1*glyph.width)
+        position.x += self.horiz_adv_x.get(character, glyph.width)
 
         return node
 
