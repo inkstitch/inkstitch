39daddb71a279850d41364a3ff950ba77a1c3e0b
LaureFR38 <63351363+LaureFR38@users.noreply.github.com>
2020-10-14 09:50:14 +0200

    Update font.py

--- lib/lettering/font.py
+++ lib/lettering/font.py
@@ -1,15 +1,4 @@
 # -*- coding: UTF-8 -*-
-
-import gettext
-
-try:
-    from plot_utils_import import from_dependency_import
-    inkex = from_dependency_import('ink_extensions.inkex')
-    serial = from_dependency_import('serial')
-except:
-    import inkex
-    import serial
-
 from copy import deepcopy
 import json
 import os
