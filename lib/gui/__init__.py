ace5d1fd55d0e9261878fedc176eeb2e08c10558
Lex Neva <lexelby@users.noreply.github.com>
2021-04-15 21:32:15 -0400

    Revert "output a warning in params if simulation cannot load (#1143)"
    This reverts commit 7ce3c8e7a04b86163f3b9f0a206b135411e3293b.

--- lib/gui/__init__.py
+++ lib/gui/__init__.py
@@ -7,4 +7,3 @@ from .dialogs import confirm_dialog, info_dialog
 from .electron import open_url
 from .presets import PresetsPanel
 from .simulator import EmbroiderySimulator, SimulatorPreview, show_simulator
-from .warnings import WarningPanel
--- lib/gui/dialogs.py
+++ lib/gui/dialogs.py
@@ -13,7 +13,7 @@ def confirm_dialog(parent, question, caption='ink/stitch'):
     return result
 
 
-def info_dialog(parent, message, caption='Ink/Stitch'):
+def info_dialog(parent, message, caption='ink/stitch'):
     dlg = wx.MessageDialog(parent, message, caption, wx.OK | wx.ICON_INFORMATION)
     dlg.ShowModal()
     dlg.Destroy()
