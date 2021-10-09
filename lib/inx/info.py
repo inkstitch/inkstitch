241383fef3503e6d60d4e172318e374a0857b4c9
Kaalleen <reni@allenka.de>
2021-10-09 13:15:58 +0200

    ...

--- lib/inx/info.py
+++ lib/inx/info.py
@@ -8,7 +8,7 @@ from .utils import build_environment, write_inx_file
 
 def generate_info_inx_files():
     env = build_environment()
-    info_inx_files = ['about', 'embroider']
+    info_inx_files = ['about']
     for info in info_inx_files:
         template = env.get_template('%s.xml' % info)
         write_inx_file(info, template.render())
