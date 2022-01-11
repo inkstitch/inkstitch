bb8f0c128456a8c248c77ab052c49a2de0a3e823
Kaalleen <reni@allenka.de>
2022-01-11 19:00:27 +0100

    inkscape config path

--- lib/utils/inkscape.py
+++ lib/utils/inkscape.py
@@ -4,18 +4,14 @@
 # Licensed under the GNU GPL version 3.0 or later.  See the file LICENSE for details.
 
 import sys
-from os.path import expanduser, realpath
+from os.path import expanduser, split
+
+from inkex.utils import get_user_directory
 
 
 def guess_inkscape_config_path():
     if getattr(sys, 'frozen', None):
-        path = realpath(sys._MEIPASS.split('extensions', 1)[0])
-        if sys.platform == "win32":
-            import win32api
-
-            # This expands ugly things like EXTENS~1
-            path = win32api.GetLongPathName(path)
+        path = split(get_user_directory())[0]
     else:
         path = expanduser("~/.config/inkscape")
-
     return path
