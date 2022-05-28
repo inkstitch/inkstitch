c1849d3c800b712166eacf3054e29c15cb6c33a3
Kaalleen <reni@allenka.de>
2022-05-28 05:42:36 +0200

    update get guide line in elements stroke

--- lib/elements/stroke.py
+++ lib/elements/stroke.py
@@ -377,7 +377,8 @@ class Stroke(EmbroideryElement):
     def _get_guide_lines(self, multiple=False):
         guide_lines = get_marker_elements(self.node, "guide-line", False, True, True)
         # No or empty guide line
-        if not guide_lines or (not guide_lines['stroke'] and not guide_lines['satins']):
+        # if there is a satin guide line, it will also be in stroke, so no need to check for satin here
+        if not guide_lines or not guide_lines['stroke']:
             return None
 
         if multiple:
@@ -386,6 +387,8 @@ class Stroke(EmbroideryElement):
             else:
                 return False
 
+        # use the satin guide line if there is one, else use stroke
+        # ignore multiple guide lines
         if len(guide_lines['satin']) >= 1:
             return guide_lines['satin'][0]
         return guide_lines['stroke'][0]
