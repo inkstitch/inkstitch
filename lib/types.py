dfee6d59e5b27347356ceae7d8700169b8d5aa58
Jeremy Kun <j2kun@users.noreply.github.com>
2022-10-31 22:15:37 -0700

    fix the type definitions

--- lib/types.py
+++ lib/types.py
@@ -1,7 +1,19 @@
 from typing import List
+
+try:
+    from typing import TypeAlias  # type: ignore
+except ImportError:
+    # for Python < 3.10
+    from typing_extensions import TypeAlias  # type: ignore
+
 import shapely
 
-"""The datatype representing a 2D shape in the plane."""
-Shape = shapely.geometry.MultiPolygon
+"""The datatype representing a connected 2D shape in the plane."""
+Shape = shapely.geometry.Polygon
 
-GratingSegments = List[List[shapely.coords.CoordinateSequence]]
+"""A segment is a sequence of two coordinates representing the start and end of
+the segment."""
+Segment: TypeAlias = shapely.coords.CoordinateSequence
+
+"""A list of possibly disconnected segments on the same row."""
+RowSegments = List[Segment]
