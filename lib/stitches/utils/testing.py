d0a00b00afe2624e9d6d179a57273b5fcc55c2a3
Jeremy Kun <j2kun@users.noreply.github.com>
2022-11-06 21:01:03 -0800

    make assert_close more general

--- lib/stitches/utils/testing.py
+++ lib/stitches/utils/testing.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple
+from typing import List, Tuple, Any
+from collections.abc import Iterable
 
 from lib.types import RowSegments
 from lib.utils.geometry import Point
@@ -15,3 +16,22 @@ def as_point_list(rows: List[RowSegments]) -> List[List[Tuple[Point, Point]]]:
         point_list.append(converted_segments)
 
     return point_list
+
+
+def assert_close(expected: Any, actual: Any) -> None:
+    """
+    Assert two arbitrarily-nested Iterables of Points are identical in
+    structure and close in value.
+    """
+    if isinstance(expected, Iterable):
+        assert isinstance(actual, Iterable)
+        expected_list = list(iter(expected))
+        actual_list = list(iter(actual))
+        assert len(expected_list) == len(actual_list)
+        for (expected_value, actual_value) in zip(expected_list, actual_list):
+            assert_close(expected_value, actual_value)
+    elif isinstance(expected, Point):
+        assert isinstance(actual, Point)
+        assert expected.isclose(actual)
+    else:
+        assert False, f"Arguments had unknown types {type(expected)}, {type(actual)}"
