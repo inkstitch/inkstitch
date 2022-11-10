ee6b311bfd7f03ce04c1de699b338e9852282bac
Jeremy Kun <j2kun@users.noreply.github.com>
2022-11-09 21:17:18 -0800

    remove assert_close in favor of DeepDiff

--- lib/stitches/fill_test.py
+++ lib/stitches/fill_test.py
@@ -1,4 +1,5 @@
 from shapely import geometry as shgeo
+from deepdiff import DeepDiff
 
 from lib.stitches import fill
 from lib.stitches.utils import testing
@@ -22,20 +23,19 @@ def test_intersect_region_with_grating_convex():
     # bottom of the box, and are oriented from right to left.
     # The very bottom edge is not included as a stitch.
     expected = [
-        [(Point(20, 11), Point(10, 11))],
-        [(Point(20, 12), Point(10, 12))],
-        [(Point(20, 13), Point(10, 13))],
-        [(Point(20, 14), Point(10, 14))],
-        [(Point(20, 15), Point(10, 15))],
-        [(Point(20, 16), Point(10, 16))],
-        [(Point(20, 17), Point(10, 17))],
-        [(Point(20, 18), Point(10, 18))],
-        [(Point(20, 19), Point(10, 19))],
-        [(Point(20, 20), Point(10, 20))],
+        [(Point(20., 11.), Point(10., 11.))],
+        [(Point(20., 12.), Point(10., 12.))],
+        [(Point(20., 13.), Point(10., 13.))],
+        [(Point(20., 14.), Point(10., 14.))],
+        [(Point(20., 15.), Point(10., 15.))],
+        [(Point(20., 16.), Point(10., 16.))],
+        [(Point(20., 17.), Point(10., 17.))],
+        [(Point(20., 18.), Point(10., 18.))],
+        [(Point(20., 19.), Point(10., 19.))],
+        [(Point(20., 20.), Point(10., 20.))],
     ]
 
-    assert len(actual) == len(expected)
-    testing.assert_close(expected, actual)
+    assert not DeepDiff(expected, actual, significant_digits=5)
 
 
 def test_intersect_region_with_grating_H_shape():
@@ -55,19 +55,18 @@ def test_intersect_region_with_grating_H_shape():
     actual = testing.as_point_list(actual)
 
     expected = [
-        [(Point(12, 11), Point(10, 11)), (Point(20, 11), Point(18, 11))],
-        [(Point(12, 12), Point(10, 12)), (Point(20, 12), Point(18, 12))],
-        [(Point(12, 13), Point(10, 13)), (Point(20, 13), Point(18, 13))],
-        [(Point(12, 14), Point(10, 14)), (Point(20, 14), Point(18, 14))],
+        [(Point(12., 11.), Point(10., 11.)), (Point(20., 11.), Point(18., 11.))],
+        [(Point(12., 12.), Point(10., 12.)), (Point(20., 12.), Point(18., 12.))],
+        [(Point(12., 13.), Point(10., 13.)), (Point(20., 13.), Point(18., 13.))],
+        [(Point(12., 14.), Point(10., 14.)), (Point(20., 14.), Point(18., 14.))],
         # these two rows are in the "belt", and so their intersection with
         # the region is a single connected segment.
-        [(Point(20, 15), Point(10, 15))],
-        [(Point(20, 16), Point(10, 16))],
-        [(Point(12, 17), Point(10, 17)), (Point(20, 17), Point(18, 17))],
-        [(Point(12, 18), Point(10, 18)), (Point(20, 18), Point(18, 18))],
-        [(Point(12, 19), Point(10, 19)), (Point(20, 19), Point(18, 19))],
-        [(Point(12, 20), Point(10, 20)), (Point(20, 20), Point(18, 20))],
+        [(Point(20., 15.), Point(10., 15.))],
+        [(Point(20., 16.), Point(10., 16.))],
+        [(Point(12., 17.), Point(10., 17.)), (Point(20., 17.), Point(18., 17.))],
+        [(Point(12., 18.), Point(10., 18.)), (Point(20., 18.), Point(18., 18.))],
+        [(Point(12., 19.), Point(10., 19.)), (Point(20., 19.), Point(18., 19.))],
+        [(Point(12., 20.), Point(10., 20.)), (Point(20., 20.), Point(18., 20.))],
     ]
 
-    assert len(actual) == len(expected)
-    testing.assert_close(expected, actual)
+    assert not DeepDiff(expected, actual, significant_digits=5)
