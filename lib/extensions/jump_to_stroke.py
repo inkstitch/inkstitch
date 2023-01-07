af2dd87b8bb78c0d57e35a4ee2692058a9c4d0c8
Kaalleen <reni@allenka.de>
2023-01-07 08:49:36 +0100

    jump to stroke: add stitch length settings

--- lib/extensions/jump_to_stroke.py
+++ lib/extensions/jump_to_stroke.py
@@ -1,18 +1,24 @@
 # Authors: see git history
 #
-# Copyright (c) 2010 Authors
+# Copyright (c) 2023 Authors
 # Licensed under the GNU GPL version 3.0 or later.  See the file LICENSE for details.
 
 from inkex import DirectedLineSegment, PathElement, errormsg
 
 from ..i18n import _
 from ..svg import PIXELS_PER_MM, get_correction_transform
+from ..svg.tags import INKSTITCH_ATTRIBS
 from .base import InkstitchExtension
 
 
 class JumpToStroke(InkstitchExtension):
     """Adds a running stitch as a connection between two (or more) selected elements.
        The elements must have the same color and a minimum distance (collapse_len)."""
+
+    def __init__(self, *args, **kwargs):
+        InkstitchExtension.__init__(self, *args, **kwargs)
+        self.arg_parser.add_argument("-l", "--stitch-length", type=float, default=2.5, dest="running_stitch_length_mm")
+        self.arg_parser.add_argument("-t", "--tolerance", type=float, default=2.0, dest="running_stitch_tolerance_mm")
 
     def effect(self):
         if not self.svg.selection or not self.get_elements() or len(self.elements) < 2:
@@ -49,6 +55,8 @@ class JumpToStroke(InkstitchExtension):
         style = f'stroke:{color};stroke-width:1px;stroke-dasharray:3, 1;fill:none;'
 
         line = PathElement(d=path, style=style, transform=get_correction_transform(node))
+        line.set(INKSTITCH_ATTRIBS['running_stitch_length_mm'], self.options.running_stitch_length_mm)
+        line.set(INKSTITCH_ATTRIBS['running_stitch_tolerance_mm'], self.options.running_stitch_tolerance_mm)
         parent.insert(index, line)
 
 
