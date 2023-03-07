9552291de5c7f1236542e9be3f1816e29701b7ed
Kaalleen <36401965+kaalleen@users.noreply.github.com>
2023-03-07 17:52:09 +0100

    change method to update legacy fill_method (#2111)

--- lib/utils/param.py
+++ lib/utils/param.py
@@ -1,9 +1,8 @@
 class ParamOption:
-    def __init__(self, param_id=None, name=None, legacy=None, preview_image=None):
+    def __init__(self, param_id=None, name=None, preview_image=None):
         self.id: str = param_id
         self.name: str = name
-        self.legacy: int = legacy
-        self.preview_image: str = None
+        self.preview_image: str = preview_image
 
     def __repr__(self):
-        return "ParamOption(%s, %s, %s, %s)" % (self.id, self.name, self.legacy, self.preview_image)
+        return "ParamOption(%s, %s, %s)" % (self.id, self.name, self.preview_image)
