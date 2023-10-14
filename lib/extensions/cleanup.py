dbdb7bd7e198c1d6104935031e03a2560ad1d954
Kaalleen <reni@allenka.de>
2023-10-14 08:13:59 +0200

    cleanup document: add options to remove empty groups/layers

--- lib/extensions/cleanup.py
+++ lib/extensions/cleanup.py
@@ -7,6 +7,7 @@ from inkex import NSS, Boolean, errormsg
 
 from ..elements import FillStitch, Stroke
 from ..i18n import _
+from ..svg.tags import SVG_GROUP_TAG
 from .base import InkstitchExtension
 
 
@@ -17,12 +18,14 @@ class Cleanup(InkstitchExtension):
         self.arg_parser.add_argument("-s", "--rm_stroke", dest="rm_stroke", type=Boolean, default=True)
         self.arg_parser.add_argument("-a", "--fill_threshold", dest="fill_threshold", type=int, default=20)
         self.arg_parser.add_argument("-l", "--stroke_threshold", dest="stroke_threshold", type=int, default=5)
+        self.arg_parser.add_argument("-g", "--rm_groups", dest="rm_groups", type=Boolean, default=True)
 
     def effect(self):
         self.rm_fill = self.options.rm_fill
         self.rm_stroke = self.options.rm_stroke
         self.fill_threshold = self.options.fill_threshold
         self.stroke_threshold = self.options.stroke_threshold
+        self.rm_groups = self.options.rm_groups
 
         self.svg.selection.clear()
 
@@ -38,12 +41,21 @@ class Cleanup(InkstitchExtension):
             return
 
         for element in self.elements:
-            if (isinstance(element, FillStitch) and self.rm_fill and element.shape.area < self.fill_threshold):
+            if self.rm_fill and (isinstance(element, FillStitch) and element.shape.area < self.fill_threshold):
                 element.node.getparent().remove(element.node)
                 count += 1
-            if (isinstance(element, Stroke) and self.rm_stroke and
+            if self.rm_stroke and (isinstance(element, Stroke) and
                element.shape.length < self.stroke_threshold and element.node.getparent() is not None):
                 element.node.getparent().remove(element.node)
                 count += 1
 
         errormsg(_("%s elements removed" % count))
+
+        count = 0
+        if self.rm_groups:
+            for group in self.svg.iterdescendants(SVG_GROUP_TAG):
+                if len(group.getchildren()) == 0:
+                    group.getparent().remove(group)
+                    count += 1
+
+            errormsg(_("%s groups/layers removed" % count))
