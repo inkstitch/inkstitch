c8189b3887c4d4e84f77baeb647cfaff4d90b26e
Kaalleen <reni@allenka.de>
2023-10-23 20:17:48 +0200

    obey min stitch length setting

--- lib/extensions/density_map.py
+++ lib/extensions/density_map.py
@@ -38,8 +38,9 @@ class DensityMap(InkstitchExtension):
 
         self.metadata = self.get_inkstitch_metadata()
         collapse_len = self.metadata['collapse_len_mm']
+        min_stitch_len = self.metadata['min_stitch_len_mm']
         patches = self.elements_to_stitch_groups(self.elements)
-        stitch_plan = stitch_groups_to_stitch_plan(patches, collapse_len=collapse_len)
+        stitch_plan = stitch_groups_to_stitch_plan(patches, collapse_len=collapse_len, min_stitch_len=min_stitch_len)
 
         layer = svg.find(".//*[@id='__inkstitch_density_plan__']")
         color_groups = create_color_groups(layer)
