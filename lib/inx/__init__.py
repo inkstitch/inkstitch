2be2dd6c6d2abab98ae2ade36c21e60de05102ac
karnigen <karnigen@gmail.com>
2024-02-07 21:03:57 +0100

    support for modified id and menu

--- lib/inx/__init__.py
+++
@@ -1,6 +0,0 @@
-# Authors: see git history
-#
-# Copyright (c) 2010 Authors
-# Licensed under the GNU GPL version 3.0 or later.  See the file LICENSE for details.
-
-from .generate import generate_inx_files
