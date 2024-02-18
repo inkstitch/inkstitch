62cfb948b0c57eccafe2e0d9d86352202c83e3c0
rejbasket <rejbasket@users.noreply.github.com>
2024-02-18 13:26:08 +0100

    fixed errors in batch cmd

--- bin/build-geos-win.cmd
+++ bin/build-geos-win.cmd
@@ -1,8 +1,10 @@
 @echo off
-mkdir all
-SET GEOS_INSTALL=C:\Users\foo\Downloads\all
+mkdir %CD%\geos-build
+
+SET GEOS_INSTALL=%CD%\geos-build
 if NOT DEFINED BUILD32 (SET BUILDFLAG="x64") else (SET BUILDFLAG="Win32")
 echo %BUILDFLAG%
+
 curl -O https://download.osgeo.org/geos/geos-3.12.1.tar.bz2
 
 7z x geos-3.12.1.tar.bz2
@@ -19,8 +21,9 @@ SET PATH=%GEOS_INSTALL%\bin;%PATH%
 SET GEOS_INCLUDE_PATH=%GEOS_INSTALL%\include
 set GEOS_LIBRARY_PATH=%GEOS_INSTALL%\lib
 
+xcopy %GEOS_INSTALL%\bin\geos_c.dll %pythonLocation% /E /H /C /I
+xcopy %GEOS_INSTALL%\bin\geos.dll %pythonLocation% /E /H /C /I
+
+python -m pip uninstall -y shapely
+python -m pip cache remove shapely
 python -m pip install -v shapely --no-binary shapely
-
-xcopy %GEOS_INSTALL%\bin\geos_c.dll "C:\Program Files (x86)\Python38-32" /E /H /C /I
-
-xcopy %GEOS_INSTALL%\bin\geos.dll "C:\Program Files (x86)\Python38-32" /E /H /C /I
