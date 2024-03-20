73beec7c6a65823d8cae86cd0e3db629cf06fbce
CapellanCitizen <thecapellancitizen@gmail.com>
2024-03-19 22:39:36 -0400

    Clone unit tests and rework

--- lib/elements/clone.py
+++ lib/elements/clone.py
@@ -3,8 +3,10 @@
 # Copyright (c) 2010 Authors
 # Licensed under the GNU GPL version 3.0 or later.  See the file LICENSE for details.
 
-from math import degrees
+from math import degrees, radians, sin, cos
 from copy import deepcopy
+from contextlib import contextmanager
+from typing import Generator
 
 from inkex import Transform, BaseElement
 from shapely import MultiLineString
@@ -78,14 +80,34 @@ class Clone(EmbroideryElement):
         return elements
 
     def to_stitch_groups(self, last_patch=None) -> list[StitchGroup]:
-        patches = []
+        with self.clone_elements() as elements:
+            patches = []
 
+            for element in elements:
+                stitch_groups = element.to_stitch_groups(last_patch)
+                if len(stitch_groups):
+                    last_patch = stitch_groups[-1]
+                    patches.extend(stitch_groups)
+
+            return patches
+
+    @contextmanager
+    def clone_elements(self) -> Generator[list[EmbroideryElement], None, None]:
+        """
+        A context manager method which yields a set of elements representing the cloned element(s) href'd by this clone's element.
+        Cleans up after itself afterwards.
+        This is broken out from to_stitch_groups for testing convenience, primarily.
+        Could possibly be refactored into just a generator - being a context manager is mainly to control the lifecycle of the elements
+        that are cloned (again, for testing convenience primarily)
+        """
         source_node: BaseElement = get_clone_source(self.node)
         if source_node.tag not in EMBROIDERABLE_TAGS and source_node.tag != SVG_GROUP_TAG:
-            return []
+            yield []
+            return
 
         # Effectively, manually clone the href'd element: Place it into the tree at the same location
         # as the use element this Clone represents, with the same transform
+        source_node: BaseElement = get_clone_source(self.node)
         parent: BaseElement = self.node.getparent()
         cloned_node = deepcopy(source_node)
         parent.add(cloned_node)
@@ -95,21 +117,12 @@ class Clone(EmbroideryElement):
 
             cloned_node.set('transform', Transform(self.node.get('transform')) @ Transform(cloned_node.get('transform')))
 
-            flip_angle = self.flip_angle
-
             if self.clone_fill_angle is None:
                 # Calculate the rotation angle to apply to the fill of cloned elements, if not explicitly set
                 source_transform = source_node.composed_transform()
                 cloned_transform = cloned_node.composed_transform()
-
-                # Flip the angle if the clone is flipped relative to the source element
-                flip_angle = flip_angle ^ is_transform_flipped(source_transform) ^ is_transform_flipped(cloned_transform)
-
-                source_angle = angle_for_transform(source_transform)
-                cloned_angle = angle_for_transform(cloned_transform)
-                angle = cloned_angle - source_angle
-                if flip_angle:
-                    angle = -angle
+                angle_transform = cloned_transform @ -source_transform
+                angle_transform = Transform((angle_transform.a, angle_transform.b, angle_transform.c, angle_transform.d, 0.0, 0.0))
 
             elements = self.clone_to_elements(cloned_node)
             for element in elements:
@@ -117,24 +130,24 @@ class Clone(EmbroideryElement):
                 # we run into issues due to those methods' use of caching not updating if the underlying param value is changed.
 
                 if self.clone_fill_angle is None:  # Normally, rotate the cloned element's angle by the clone's rotation.
-                    element_angle = float(element.node.get(INKSTITCH_ATTRIBS['angle'], 0)) - angle
+                    element_angle = float(element.node.get(INKSTITCH_ATTRIBS['angle'], 0)) 
+                    # We have to negate the angle because SVG/Inkscape's definition of rotation is clockwise, while Inkstitch uses counter-clockwise
+                    fill_vector = (angle_transform @ Transform(f"rotate(${-element_angle})")).apply_to_point((1,0))
+                    # Same reason for negation here.
+                    element_angle = -degrees(fill_vector.angle)
                 else:  # If clone_fill_angle is specified, override the angle instead.
                     element_angle = self.clone_fill_angle
 
-                if flip_angle:
+                if self.flip_angle:
                     element_angle = -element_angle
 
                 element.node.set(INKSTITCH_ATTRIBS['angle'], element_angle)
 
-                stitch_groups = element.to_stitch_groups(last_patch)
-                if len(stitch_groups):
-                    last_patch = stitch_groups[-1]
-                    patches.extend(stitch_groups)
-
-            return patches
+            yield elements
         finally:
             # Remove the "manually cloned" tree.
             parent.remove(cloned_node)
+
 
     @property
     def shape(self):
@@ -170,12 +183,9 @@ def angle_for_transform(t: Transform) -> float:
 
     # Strip out the translation component from the matrix
     t = Transform((t.a, t.b, t.c, t.d, 0, 0))
-    try:
-        return t.rotation_degrees()
-    except ValueError:
-        # Something is weird about the matrix such that it apparently isn't a rotation with a uniform scale.
-        # So, next best thing is to apply it to a unit vector and pull the angle from that.
-        return degrees(t.apply_to_point((1, 0)).angle)
+    if is_transform_flipped(t):
+        t = t.add_scale(-1, 1)
+    return degrees(t.apply_to_point((1, 0)).angle)
 
 
 def is_transform_flipped(t: Transform) -> bool:
---
+++ tests/test_clone.py
@@ -0,0 +1,232 @@
+from lib.elements import Clone, EmbroideryElement
+from lib.stitch_plan import StitchGroup
+from lib.svg.tags import INKSTITCH_ATTRIBS
+from inkex import SvgDocumentElement, Rectangle, Circle, Group, Use, Transform, TextElement
+from inkex.tester import TestCase
+from inkex.tester.svg import svg
+
+from math import sqrt
+
+def element_fill_angle(element: EmbroideryElement) -> float | None:
+    angle = element.node.get(INKSTITCH_ATTRIBS['angle'])
+    if angle is not None:
+        angle = float(angle) % 180
+    return angle
+
+class CloneElementTest(TestCase):
+    def assertAngleAlmostEqual(self, a, b):
+        self.assertAlmostEqual(a%180, b%180, 4)
+
+    def test_not_embroiderable(self):
+        root: SvgDocumentElement = svg()
+        text = root.add(TextElement())
+        text.text = "Can't embroider this!"
+        use = root.add(Use())
+        use.href = text
+        
+        clone = Clone(use)
+        stitch_groups = clone.to_stitch_groups(None)
+        self.assertEqual(len(stitch_groups), 0)
+
+
+    # These tests make sure the element cloning works as expected, using the `clone_elements` method.
+
+    def test_basic(self):
+        root: SvgDocumentElement = svg()
+        rect = root.add(Rectangle(attrib={
+            "width": "10",
+            "height": "10",
+            INKSTITCH_ATTRIBS["angle"]: "30" 
+        }))
+        use = root.add(Use())
+        use.href = rect
+
+        clone = Clone(use)
+        with clone.clone_elements() as elements:
+            self.assertEqual(len(elements), 1)
+            self.assertAlmostEqual(element_fill_angle(elements[0]), 30)
+
+    def test_angle_rotated(self):
+        root: SvgDocumentElement = svg()
+        rect = root.add(Rectangle(attrib={
+            "width": "10",
+            "height": "10",
+            INKSTITCH_ATTRIBS["angle"]: "30" 
+        }))
+        use = root.add(Use())
+        use.href = rect
+        use.set('transform', Transform().add_rotate(20))
+
+        clone = Clone(use)
+        with clone.clone_elements() as elements:
+            self.assertEqual(len(elements), 1)
+            self.assertAngleAlmostEqual(element_fill_angle(elements[0]), 10)
+
+    def test_angle_flipped(self):
+        root: SvgDocumentElement = svg()
+        rect = root.add(Rectangle(attrib={
+            "width": "10",
+            "height": "10",
+            INKSTITCH_ATTRIBS["angle"]: "30" 
+        }))
+        use = root.add(Use())
+        use.href = rect
+        use.set('transform', Transform().add_scale(-1, 1))
+
+        clone = Clone(use)
+        with clone.clone_elements() as elements:
+            self.assertEqual(len(elements), 1)
+            self.assertAngleAlmostEqual(element_fill_angle(elements[0]), -30)
+
+    def test_angle_flipped_rotated(self):
+        root: SvgDocumentElement = svg()
+        rect = root.add(Rectangle(attrib={
+            "width": "10",
+            "height": "10",
+            INKSTITCH_ATTRIBS["angle"]: "30" 
+        }))
+        use = root.add(Use())
+        use.href = rect
+        use.set('transform', Transform().add_rotate(20).add_scale(-1, 1))
+
+        clone = Clone(use)
+        with clone.clone_elements() as elements:
+            self.assertEqual(len(elements), 1)
+            # Fill angle goes from 30 -> -30 after flip -> -50 after rotate
+            self.assertAngleAlmostEqual(element_fill_angle(elements[0]), -50)
+
+    def test_angle_non_uniform_scale(self):
+        """
+        The angle isn't *as* well-defined for non-rotational scales, but we try to follow how the slope will be altered.
+        """
+        root: SvgDocumentElement = svg()
+        rect = root.add(Rectangle(attrib={
+            "width": "10",
+            "height": "10",
+            INKSTITCH_ATTRIBS["angle"]: "30" 
+        }))
+        use = root.add(Use())
+        use.href = rect
+        use.set('transform', Transform().add_rotate(10).add_scale(1, -sqrt(3)))
+
+        clone = Clone(use)
+        with clone.clone_elements() as elements:
+            self.assertEqual(len(elements), 1)
+            # Slope of the stitching goes from tan(30deg) = 1/sqrt(3) to -sqrt(3)/sqrt(3) = tan(-45deg),
+            # then rotated another -10 degrees to -55
+            self.assertAngleAlmostEqual(element_fill_angle(elements[0]), -55)
+
+    def test_transform_inherits_from_cloned_element(self):
+        """
+        Elements cloned by cloned_elements need to inherit their transform from their href'd element and their use to match what's shown.
+        """
+        root: SvgDocumentElement = svg()
+        rect = root.add(Rectangle(attrib={
+            "width": "10",
+            "height": "10",
+            INKSTITCH_ATTRIBS["angle"]: "30",
+        }))
+        rect.set('transform', Transform().add_scale(2,2))
+        use = root.add(Use())
+        use.href = rect
+        use.set('transform', Transform().add_translate((5, 10)))
+
+        clone = Clone(use)
+        with clone.clone_elements() as elements:
+            self.assertEqual(len(elements), 1)
+            self.assertTransformEqual(
+                elements[0].node.composed_transform(),
+                Transform().add_translate((5, 10)).add_scale(2,2))
+
+    def test_transform_inherits_from_tree(self):
+        root: SvgDocumentElement = svg()
+        g1 = root.add(Group())
+        g1.set('transform', Transform().add_translate((0, 5)).add_rotate(5))
+        rect = g1.add(Rectangle(attrib={
+            "width": "10",
+            "height": "10",
+            INKSTITCH_ATTRIBS["angle"]: "30",
+        }))
+        rect.set('transform', Transform().add_scale(2,2))
+        use = root.add(Use())
+        use.href = g1
+        use.set('transform', Transform().add_translate((5, 10)))
+
+        clone = Clone(use)
+        with clone.clone_elements() as elements:
+            self.assertEqual(len(elements), 1)
+            self.assertTransformEqual(
+                elements[0].node.composed_transform(),
+                Transform().add_translate((5, 10))  # use
+                    .add_translate((0, 5)).add_rotate(5)  # g1
+                    .add_scale(2,2),  # rect
+                5)
+
+    def test_transform_inherits_from_tree_up_tree(self):
+        root: SvgDocumentElement = svg()
+        g1 = root.add(Group())
+        g1.set('transform', Transform().add_translate((0, 5)).add_rotate(5))
+        rect = g1.add(Rectangle(attrib={
+            "width": "10",
+            "height": "10",
+            INKSTITCH_ATTRIBS["angle"]: "30",
+        }))
+        rect.set('transform', Transform().add_scale(2,2))
+        circ = g1.add(Circle())
+        circ.radius = 5
+        g2 = root.add(Group())
+        g2.set('transform', Transform().add_translate((1,2)).add_scale(0.5, 1))
+        use = g2.add(Use())
+        use.href = g1
+        use.set('transform', Transform().add_translate((5, 10)))
+
+        clone = Clone(use)
+        with clone.clone_elements() as elements:
+            self.assertEqual(len(elements), 2)
+            self.assertTransformEqual(
+                elements[0].node.composed_transform(),
+                Transform().add_translate((1,2)).add_scale(0.5, 1)  # g2 
+                    .add_translate((5, 10))  # use
+                    .add_translate((0, 5)).add_rotate(5)  # g1
+                    .add_scale(2,2),  # rect
+                5)
+            self.assertTransformEqual(
+                elements[1].node.composed_transform(),
+                Transform().add_translate((1,2)).add_scale(0.5, 1)  # g2 
+                    .add_translate((5, 10))  # use
+                    .add_translate((0, 5)).add_rotate(5),  # g1
+                5)
+
+    def test_clone_fill_angle_not_specified(self):
+        root: SvgDocumentElement = svg()
+        rect = root.add(Rectangle(attrib={
+            "width": "10",
+            "height": "10",
+            INKSTITCH_ATTRIBS["angle"]: "30" 
+        }))
+        use = root.add(Use())
+        use.href = rect
+        use.set('transform', Transform().add_rotate(20))
+
+        clone = Clone(use)
+        self.assertEqual(clone.clone_fill_angle, None)
+
+
+    def test_clone_fill_angle(self):
+        root: SvgDocumentElement = svg()
+        rect = root.add(Rectangle(attrib={
+            "width": "10",
+            "height": "10",
+            INKSTITCH_ATTRIBS["angle"]: "30" 
+        }))
+        use = root.add(Use())
+        use.href = rect
+        use.set(INKSTITCH_ATTRIBS["angle"], 42)
+        use.set('transform', Transform().add_rotate(20))
+
+        clone = Clone(use)
+        self.assertEqual(clone.clone_fill_angle, 42)
+
+        with clone.clone_elements() as elements:
+            self.assertEqual(len(elements), 1)
+            self.assertAngleAlmostEqual(element_fill_angle(elements[0]), 42)
