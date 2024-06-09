a8dcd1fc039eb29ed2c77765660b03038569401b
rejbasket <39080670+rejbasket@users.noreply.github.com>
2024-06-09 13:10:57 +0200

    Rejbasket/version geos update (#2979)
    * update geos version for linux an win builds
    
    * pinned pyinstaller version
    
    * changed windows build runner to windows-2019
    
    * changed build version for cmake geos build script
    
    ---------
    
    Co-authored-by: rejbasket <rejbasket@users.noreply.github.com>

--- bin/build-geos-win.cmd
+++ bin/build-geos-win.cmd
@@ -5,13 +5,13 @@ SET GEOS_INSTALL=%CD%\geos-build
 if NOT DEFINED BUILD32 (SET BUILDFLAG="x64") else (SET BUILDFLAG="Win32")
 echo %BUILDFLAG%
 
-curl -L -O  https://github.com/libgeos/geos/releases/download/3.12.1/geos-3.12.1.tar.bz2
+curl -L -O  https://github.com/libgeos/geos/releases/download/3.12.2/geos-3.12.2.tar.bz2
 
-7z x geos-3.12.1.tar.bz2
-7z x geos-3.12.1.tar
+7z x geos-3.12.2.tar.bz2
+7z x geos-3.12.2.tar
 
-cd geos-3.12.1
-cmake -S . -B _build -G "Visual Studio 17 2022" -A %BUILDFLAG% -DCMAKE_INSTALL_PREFIX=%GEOS_INSTALL% -DCMAKE_GENERATOR_TOOLSET=host=x64
+cd geos-3.12.2
+cmake -S . -B _build -G "Visual Studio 16 2019" -A %BUILDFLAG% -DCMAKE_INSTALL_PREFIX=%GEOS_INSTALL% -DCMAKE_GENERATOR_TOOLSET=host=x64
 
 cmake --build _build --config Release -j 16 --verbose
 cd _build
