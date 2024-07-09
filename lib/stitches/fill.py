6c9b902d15dbe2415d25e3c5aeb120f502f03548
Kaalleen <36401965+kaalleen@users.noreply.github.com>
2024-07-09 18:26:47 +0200

    Skip stitch zero length row (#3054)
    * fix #3052
    * add one stitch for zero length row

--- lib/stitches/fill.py
+++ lib/stitches/fill.py
@@ -54,6 +54,10 @@ def adjust_stagger(stitch, angle, row_spacing, max_stitch_length, staggers):
 
 def stitch_row(stitches, beg, end, angle, row_spacing, max_stitch_length, staggers, skip_last,
                enable_random_stitch_length, random_sigma, random_seed):
+    if end == beg:
+        stitches.append(Stitch(*beg, tags=('fill_row_start',)))
+        return
+
     beg = Stitch(*beg, tags=('fill_row_start',))
     end = Stitch(*end, tags=('fill_row_start',))
     stitches.append(beg)
