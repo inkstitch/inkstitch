6f75715327730277f141c8cdb574ab5a63eb66fd
Kaalleen <reni@allenka.de>
2024-08-04 19:00:08 +0200

    fix params sim background

--- lib/gui/simulator/simulator_panel.py
+++ lib/gui/simulator/simulator_panel.py
@@ -30,6 +30,7 @@ class SimulatorPanel(wx.Panel):
         self.cp.set_drawing_panel(self.dp)
         self.vp.set_drawing_panel(self.dp)
         self.vp.set_background_color(wx.Colour(background_color))
+        self.dp.set_background_color(wx.Colour(background_color))
 
         dvSizer = wx.BoxSizer(wx.HORIZONTAL)
 
