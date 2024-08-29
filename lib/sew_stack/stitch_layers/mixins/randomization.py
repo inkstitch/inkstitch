b8869d9674e13379db0f9a855137f0c9fc6cf5a3
Lex Neva <github.com@lexneva.name>
2024-08-28 23:26:00 -0400

    add randomization button

--- lib/sew_stack/stitch_layers/mixins/randomization.py
+++ lib/sew_stack/stitch_layers/mixins/randomization.py
@@ -1,11 +1,41 @@
+import os
+from secrets import randbelow
+
 import wx.propgrid
 
 from .property_grid import Category, Property
 from ....i18n import _
+from ....utils import get_resource_dir
+
+editor_instance = None
+
+
+class RandomSeedEditor(wx.propgrid.PGTextCtrlAndButtonEditor):
+    def CreateControls(self, property_grid, property, position, size):
+        if wx.SystemSettings().GetAppearance().IsDark():
+            randomize_icon_file = 'randomize_20x20_dark.png'
+        else:
+            randomize_icon_file = 'randomize_20x20.png'
+        randomize_icon = wx.Image(os.path.join(get_resource_dir("icons"), randomize_icon_file)).ConvertToBitmap()
+        # button = wx.Button(property_grid, wx.ID_ANY, _("Re-roll"))
+        # button.SetBitmap(randomize_icon)
+
+        window_list = super().CreateControls(property_grid, property, position, size)
+        button = window_list.GetSecondary()
+        button.SetBitmap(randomize_icon)
+        button.SetLabel("")
+        return window_list
 
 
 class RandomSeedProperty(wx.propgrid.IntProperty):
-    pass
+    def DoGetEditorClass(self):
+        return wx.propgrid.PropertyGridInterface.GetEditorByName("RandomSeedEditor")
+
+    def OnEvent(self, propgrid, primaryEditor, event):
+        if event.GetEventType() == wx.wxEVT_COMMAND_BUTTON_CLICKED:
+            self.SetValue(randbelow(int(1e8)))
+            return True
+        return False
 
 
 class RandomizationMixin:
@@ -18,6 +48,13 @@ class RandomizationMixin:
     @classmethod
     @property
     def randomization_properties(cls):
+        # We have to register the editor class once. We have to save a reference
+        # to the editor to avoid letting it get garbage collected.
+        global editor_instance
+        if editor_instance is None:
+            editor_instance = RandomSeedEditor()
+            wx.propgrid.PropertyGrid.DoRegisterEditorClass(editor_instance, "RandomSeedEditor")
+
         return Category(_("Randomization")).children(
             Property("random_seed", _("Random seed"), type=RandomSeedProperty)
         )
