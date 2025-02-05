0779fb940cc0f13f96efa496c801874329445fc6
Claudine <88194877+claudinepeyrat06@users.noreply.github.com>
2025-02-05 15:34:05 +0100

    add alef maddah
    one more non connecting character

--- lib/lettering/font_variant.py
+++ lib/lettering/font_variant.py
@@ -141,8 +141,8 @@ class FontVariant(object):
 
     def isbinding(self, character):
         # after  a non binding letter a letter can only be in isol or fina shape.
-        # binding glyph only have  two shapes, isol and fina
-        non_binding_char = ['ا', 'أ', 'ﺇ', 'د', 'ذ', 'ر', 'ز', 'و']
+        # binding glyph only have  two shapes, isol and fina  
+        non_binding_char = ['ا', 'أ', 'ﺇ', 'آ', 'د', 'ذ', 'ر', 'ز', 'و']
         normalized_non_binding_char = [normalize('NFKC', letter) for letter in non_binding_char]
         return not (character in normalized_non_binding_char)
 
