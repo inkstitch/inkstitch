b009a5355e7ceea477ea55eacba2149a9398b37e
Kaalleen <36401965+kaalleen@users.noreply.github.com>
2025-04-19 07:11:57 +0200

    duplicate-consistant autoroute element selection (#3638)

--- lib/stitches/auto_satin.py
+++ lib/stitches/auto_satin.py
@@ -428,6 +428,8 @@ def _route_single_satin(elements, starting_point, keep_originals):
     stroke_width = convert_stroke_width(satin)
     run_element.node.style['stroke-width'] = stroke_width
 
+    run_element.node.set('inkstitch:path_type', 'satin-underpath')
+
     parent.insert(index, run_element.node)
     if not keep_originals:
         remove_original_elements([satin], True)
@@ -598,6 +600,7 @@ def name_elements(new_elements, preserve_order):
             element.node.set("id", generate_unique_id(element.node, "autosatin"))
         else:
             element.node.set("id", generate_unique_id(element.node, "autosatinrun"))
+            element.node.set("inkstitch:path_type", 'satin-underpath')
 
         if not (preserve_order and INKSCAPE_LABEL in element.node.attrib):
             if isinstance(element, SatinColumn):
