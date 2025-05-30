b8d8cbc3bf68051082be5256c35736117b3451be
Kaalleen <reni@allenka.de>
2025-05-30 19:39:18 +0200

    ignore bad font paths in lettering

--- lib/lettering/utils.py
+++ lib/lettering/utils.py
@@ -10,7 +10,7 @@ from ..lettering import Font
 from ..utils import get_bundled_dir, get_user_dir
 
 
-def get_font_list():
+def get_font_list(show_font_path_warning=True):
     font_paths = get_font_paths()
 
     fonts = []
@@ -21,7 +21,7 @@ def get_font_list():
             continue
 
         for font_dir in font_dirs:
-            font = _get_font_from_path(font_path, font_dir)
+            font = _get_font_from_path(font_path, font_dir, show_font_path_warning)
             if not font or font.marked_custom_font_name == "" or font.marked_custom_font_id == "":
                 continue
             fonts.append(font)
@@ -66,7 +66,7 @@ def get_font_by_name(font_name):
     return None
 
 
-def _get_font_from_path(font_path, font_dir):
+def _get_font_from_path(font_path, font_dir, show_font_path_warning=True):
     if not os.path.isdir(os.path.join(font_path, font_dir)) or font_dir.startswith('.'):
         return
-    return Font(os.path.join(font_path, font_dir))
+    return Font(os.path.join(font_path, font_dir), show_font_path_warning)
