7addf6b18f75b2b8639d901ab6ae6e5fcfb54b36
karnigen <karnigen@gmail.com>
2025-06-08 15:52:07 +0200

    rename update scripts

--- bin/uv/uv_tools_install.sh
+++ bin/uv/uv_tools_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-### install common tools  for all platforms
+### Install common tools  for all platforms
 
 ### - install `uv`:  see https://github.com/astral-sh/uv
 ### - put `uv` on PATH
@@ -12,10 +12,10 @@
 # powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 
 
-### install uvr
+### Install uvr
 uv tool install --from git+https://github.com/karnigen/uvr uvr
 
-### tools required
+### Tools required
 ### flake8 for style checking - see make style
 uv tool install flake8
 ### cmake for building C extensions
