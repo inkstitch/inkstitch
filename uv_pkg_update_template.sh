5ac2bd6ac1718a02a3324b29de66bd6a2ca300a9
karnigen <karnigen@gmail.com>
2025-06-08 15:14:04 +0200

    draft update

--- uv_pkg_update_template.sh
+++ uv_pkg_update_template.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-### !!! before running this script, install uv and the tools: see bin/uv/tools_install.sh
-### !!! run this script in the root of your project
-### !!! - copy this to your local copy
+### !!! - before running this script, install uv and the tools: see bin/uv/tools_install.sh
+### !!! - copy this to your local 
+###       > `cp uv_pkg_update_template.sh copy uv_pkg_update.sh`
+### !!! - run `./uv_pkg_update.sh` in the root of your project
 
 ### set python version, create .python-version file
 uv python pin 311
@@ -11,11 +12,8 @@ uv python pin 311
 uv venv
 
 ### wxPython for Linux - requires to check python version and OS
-# uv pip install "https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-22.04/wxpython-4.2.3-cp39-cp39-linux_x86_64.whl"
 # uv pip install "https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-22.04/wxpython-4.2.3-cp311-cp311-linux_x86_64.whl"
 
-###
-# uv pip install "pygobject<=3.50"
 
 ### Here put your packages
 # uv pip install mypkg
