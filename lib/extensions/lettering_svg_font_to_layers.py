23cb923a7cff1742fc77dc377717776ee3e56786
Claudine <88194877+claudinepeyrat06@users.noreply.github.com>
2025-06-13 19:45:57 +0200

    first steps

--- lib/extensions/lettering_svg_font_to_layers.py
+++ lib/extensions/lettering_svg_font_to_layers.py
@@ -34,18 +34,39 @@ class LetteringSvgFontToLayers(InkstitchExtension):
         pars.add_argument(
             "--count",
             type=int,
-            default=30,
-            help="Stop making layers after this number of glyphs.",
+            default=150,
+            help="Stop making layers after this number of glyphs."
+        )
+        pars.add_argument(
+            "--scale",
+            type=float,
+            default=1.0,
+            help="Scale the font (1 = no scaling)"
+        )
+    def create_horizontal_guideline(self, name: str, position):
+        """Create a horizontal guideline with name and position
+
+        Args:
+            name (str): the name of the guideline
+            position (Union[int, float]): the vertical position of the guideline
+
+        Returns:
+            inkex.BaseElement: the created guideline
+        """
+        return self.svg.namedview.add_guide(
+            self.svg.height - position, True, name
+         #    self.svg.viewbox_height - position, True, name
         )
 
     def flip_cordinate_system(self, elem, emsize, baseline):
         """Scale and translate the element's path, returns the path object"""
         path = elem.path
-        path.scale(1, -1, inplace=True)
+        path.scale(self.options.scale, -self.options.scale, inplace=True)
         path.translate(0, int(emsize) - int(baseline), inplace=True)
         return path
 
     def effect(self):
+        scale_by = self.options.scale
         # Current code only reads the first svgfont instance
         font = self.svg.defs.findone("svg:font")
         if font is None:
@@ -54,33 +75,65 @@ class LetteringSvgFontToLayers(InkstitchExtension):
         baseline = font.get("horiz-origin-y")
         if baseline is None:
             baseline = 0
+        else:
+            baseline = baseline * scale_by
 
-        guidebase = self.svg.viewbox_height - baseline
+        guidebase = (self.svg.viewbox_height * scale_by) - baseline
 
         fontface = font.findone("svg:font-face")
 
-        emsize = fontface.get("units-per-em")
+        emsize = fontface.get("units-per-em") 
+        emsize = float(emsize) * scale_by
+        self.svg.set("width", str(emsize))
+        self.svg.set("height", str(emsize))
+        self.svg.set("viewBox", "0 0 " + str(emsize) + " " + str(emsize))
 
         # TODO: should we guarantee that <svg:font horiz-adv-x> equals <svg:font-face units-per-em> ?
-        caps = float(fontface.get("cap-height", 0))
-        xheight = float(fontface.get("x-height", 0))
-        ascender = float(fontface.get("ascent", 0))
-        descender = float(fontface.get("descent", 0))
+        caps = float(fontface.get("cap-height", 0)) * scale_by  
+        xheight = float(fontface.get("x-height", 0)) * scale_by
+        ascender = float(fontface.get("ascent", 0)) * scale_by
+        descender = float(fontface.get("descent", 0)) * scale_by
+        fontface.set("cap-height",str(caps))
+        fontface.set("x-height", str(xheight))
+        fontface.set("ascent", str(ascender))
+        fontface.set("descent", str(descender))
 
-        self.svg.set("width", emsize)
-        self.svg.namedview.add_guide(guidebase, True, "baseline")
-        self.svg.namedview.add_guide(guidebase - ascender, True, "ascender")
-        self.svg.namedview.add_guide(guidebase - caps, True, "caps")
-        self.svg.namedview.add_guide(guidebase - xheight, True, "xheight")
-        self.svg.namedview.add_guide(guidebase + descender, True, "decender")
+       
 
+        baseline = descender
+        # Create guidelines
+        self.create_horizontal_guideline(_("baseline"), baseline)
+        self.create_horizontal_guideline(_("ascender"), baseline + ascender)
+        self.create_horizontal_guideline(_("caps"), baseline + caps)
+        self.create_horizontal_guideline(_("xheight"), baseline + xheight)
+        self.create_horizontal_guideline(_("descender"), baseline - descender)
+      
+
+        namedview = self.svg.namedview
+        namedview.set("inkscape:document-units", "px")
+        namedview.set("inkscape:cx", str(emsize / 2.0))
+        namedview.set("inkscape:cy", str(emsize / 2.0))
+       # self.svg.namedview.add_guide(guidebase, True, "baseline")
+        # self.svg.namedview.add_guide(guidebase - ascender, True, "ascender")
+        # self.svg.namedview.add_guide(guidebase - caps, True, "caps")
+        # self.svg.namedview.add_guide(guidebase - xheight, True, "xheight")
+        # self.svg.namedview.add_guide(guidebase + descender, True, "decender")
         count = 0
         for glyph in font.findall("svg:glyph"):
             hide_layer = count != 0
+            hax = glyph.get("horiz-adv-x", None)
+            if hax is not None:
+                hax = float(hax) * scale_by
+                glyph.set(("horiz-adv-x"), str(hax))
             self.convert_glyph_to_layer(glyph, emsize, baseline, hide_layer=hide_layer)
             count += 1
             if count >= self.options.count:
                 break
+
+
+       
+        
+        
 
     def convert_glyph_to_layer(self, glyph, emsize, baseline, hide_layer):
         unicode_char = glyph.get("unicode")
