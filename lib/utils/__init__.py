40f1b2eccfc27764e49b20bf712d4f5fa321fcac
Martin Fischer <martin@push-f.com>
2025-06-22 18:31:46 +0200

    fix: tests during Nix build
    Fixes the following error when executing the tests during a Nix build:
    
        Traceback (most recent call last):
          File "/build/inkstitch/lib/elements/element.py", line 714, in handle_unexpected_exceptions
            yield
          File "/build/inkstitch/lib/elements/element.py", line 650, in embroider
            stitch_groups = self._load_cached_stitch_groups(previous_stitch, next_element)
                            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
          File "/build/inkstitch/lib/debug/debug.py", line 172, in decorated
            result = func(*args, **kwargs)
                     ^^^^^^^^^^^^^^^^^^^^^
          File "/build/inkstitch/lib/elements/element.py", line 541, in _load_cached_stitch_groups
            stitch_groups = get_stitch_plan_cache().get(cache_key)
                            ^^^^^^^^^^^^^^^^^^^^^^^
          File "/build/inkstitch/lib/utils/cache.py", line 31, in get_stitch_plan_cache
            cache_dir = get_user_dir('cache')
                        ^^^^^^^^^^^^^^^^^^^^^
          File "/build/inkstitch/lib/utils/paths.py", line 50, in get_user_dir
            os.makedirs(path)
          File "<frozen os>", line 215, in makedirs
          File "<frozen os>", line 215, in makedirs
          File "<frozen os>", line 225, in makedirs
        PermissionError: [Errno 13] Permission denied: '/homeless-shelter'

--- lib/utils/__init__.py
+++ lib/utils/__init__.py
@@ -3,7 +3,6 @@
 # Copyright (c) 2010 Authors
 # Licensed under the GNU GPL version 3.0 or later.  See the file LICENSE for details.
 
-from . import cache as cache_module  # Slight hack to allow cache to be imported for monkeypatching
 from .cache import cache
 from .dotdict import DotDict
 from .geometry import *
