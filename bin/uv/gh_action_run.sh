080ec02dd13ce7a7f8246c042e7340c6025f5dc7
karnigen <karnigen@gmail.com>
2025-06-25 00:40:32 +0200

    simplified doc

--- bin/uv/gh_action_run.sh
+++ bin/uv/gh_action_run.sh
@@ -4,36 +4,61 @@
 # Install gh CLI: https://cli.github.com/
 # Make sure you have the necessary permissions to trigger workflows.
 
-### - Avoid frequent runs to prevent your IP address from being quickly banned.
-
-
+set -e
 set -x
 
+# options:
+#   build_type  - OS architecture
+#   input_tag   - v* version tag
+#   verbosity   - log level 0 - error, 1 - info, 2 - debug  3 - trace
+#   sign        - authorized sign fow windows and notarize for mac
+#   break_on    - break on uv, sync or no
+
 WF=uv_build.yml
+
 BR=`git rev-parse --abbrev-ref HEAD`
 
+
 build_type='dummy'
+# build_type='all'
+
 # build_type='linux32'
 # build_type='linux64'
 # build_type='linuxarm64'
-# build_type='macarm64'
 # build_type='macx86'
+# build_type='macarm64'
 # build_type='windows64'
-# build_type='all'
 
-# break_on="no"
-# break_on="uv"
-# break_on="sync" # on exit cache is not updated !!!
 
-# tag='dev-build-$BR'
+# sign: true - authorized sign for windows and notarize for mac, false (default) - no windows sign and no mac notarize
+# sign='true'
 
-# git commit -a -m "Automated Commit & Build"
-# git push
+# input_tag
+tag='v0.0.0-alpha'
 
-### gh workflow run $WF -r $BR -f build_type=$build_type
-### gh workflow run $WF -r $BR -f build_type=$build_type -f break_on=$break_on -f input_tag=$tag
+break_on="no"
+#break_on="uv"
+#break_on="sync" # on exit cache is not updated !!!
+
+# verbosity: 0 - error, 1 - info (default), 2 - debug, 3 - trace
+log_level=1
+
+
+#git commit -a -m "Automated Commit & Build"
+#git push
+
+### --------------------------------------------------------- run workflow
+
+### common options
+gh workflow run $WF -r $BR -f build_type=$build_type
+
+### for v* rebuilds
+# gh workflow run $WF -r $BR -f build_type=$build_type -f input_tag=$tag
+
+### for v* rebuilds with authorized sign
+# gh workflow run $WF -r $BR -f build_type='all' -f input_tag=$tag -f sign='true'
+
+### developer options
 # gh workflow run $WF -r $BR -f build_type=$build_type -f break_on=$break_on
-
-
-
+# gh workflow run $WF -r $BR -f build_type=$build_type -f break_on=$break_on -f verbosity=$log_level
 
