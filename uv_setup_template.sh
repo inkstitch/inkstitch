b572237fd8cbc146cf599a5cda86c61b13499544
karnigen <karnigen@gmail.com>
2025-07-23 15:54:11 +0200

    check .venv

--- uv_setup_template.sh
+++ uv_setup_template.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 ### !!! - before running this script, install uv and the tools: see bin/uv/tools_install.sh
-### !!! - copy this to your local 
+### !!! - copy this to your local
 ###       > `cp uv_setup_template.sh copy uv_setup.sh`
-### !!! - run `./uv_setup.sh` in the root of your project
+### !!! - run `./uv_setup.sh`
+
+if [ -d ".venv" ]; then
+  echo ".venv already exists, skipping setup."
+  exit 0
+fi
+
+### remove uv.lock file - to allow uv to recreate it
+# rm -f uv.lock
 
 ### set python version, create .python-version file
 uv python pin 311
@@ -11,6 +19,8 @@ uv python pin 311
 ### create virtual environment (delete .venv to recreate everything)
 uv venv
 
+### Manually add packages to pyproject.toml
+# uv pip install "pygobject<=3.50"
 ### wxPython for Linux - requires to check python version and OS
 # uv pip install "https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-22.04/wxpython-4.2.3-cp311-cp311-linux_x86_64.whl"
 
