c9d5ab81dd88c2c6e7e198f301adbfed30e70a9e
Lex Neva <github.com@lexneva.name>
2025-11-03 20:40:22 -0500

    use proper __all__

--- lib/sew_stack/stitch_layers/__init__.py
+++ lib/sew_stack/stitch_layers/__init__.py
@@ -1,7 +1,7 @@
 from .running_stitch import RunningStitchLayer
 
-all = [RunningStitchLayer]
+__all__ = [RunningStitchLayer]
 by_id = {}
 
-for layer_class in all:
+for layer_class in __all__:
     by_id[layer_class.layer_id] = layer_class
