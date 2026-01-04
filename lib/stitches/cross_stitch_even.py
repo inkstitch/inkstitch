35a536d5d1ae280cbfc9ae680e086ece1cc2f363
Claudine <88194877+claudinepeyrat06@users.noreply.github.com>
2026-01-04 20:38:57 +0100

    Update cross_stitch_even.py

--- lib/stitches/cross_stitch_even.py
+++ lib/stitches/cross_stitch_even.py
@@ -6,6 +6,8 @@
 import networkx as nx
 from .utils.cross_stitch import CrossGeometry
 
+import sys
+
 
 def even_cross_stitch(fill, shape, starting_point):
     cross_geoms = CrossGeometry(fill, shape, fill.cross_stitch_method)
@@ -13,14 +15,23 @@ def even_cross_stitch(fill, shape, starting_point):
         return []
     
     subgraphs = _build_connect_subgraph(cross_geoms)
+    print(len(subgraphs),  file=sys.stderr)
+    sys.stderr.write(f"Built {len(subgraphs)} subgraphs for cross stitch pattern.\n")
     eulerian_cycles = _build_eulerian_cycles(subgraphs)
-    import sys
-    print(f"Even cross stitch will create {len(eulerian_cycles)} separate paths", file=sys.stderr)
+    sys.stderr.write(f"Built {len(eulerian_cycles)} eulerian cycles for cross stitch pattern.\n")
+    for cycle in eulerian_cycles:
+        sys.stderr.write(f"Built eulerian cycle with {len(cycle)} edges.\n")
+        sys.stderr.write(str(cycle))
+        sys.stderr.write("\n")
+
+    stitches = _cycles_to_stitches(eulerian_cycles)
+    
     return []
 
+    
 
 def _build_connect_subgraph(cross_geoms):
-
+# do i really need to convert to strings  here?
     G = nx.Graph()
     for line in cross_geoms.travel_edges:
         G.add_edge(str(line.coords[0]), str(line.coords[-1]), path=line)
@@ -41,3 +52,8 @@ def _build_eulerian_cycles(subgraphs):
 
         eulerian_cycles.append(cycle)
     return eulerian_cycles
+
+def _cycles_to_stitches(eulerian_cycles):
+    stitches = []
+    
+    return stitches
