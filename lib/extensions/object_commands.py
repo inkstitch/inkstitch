baba4ae86be7ec451abca6c4555b1e6d7ac8b8bd
Kaalleen <36401965+kaalleen@users.noreply.github.com>
2026-01-12 17:34:29 +0100

    object commands: add position option (#4169)

--- lib/extensions/object_commands.py
+++ lib/extensions/object_commands.py
@@ -4,10 +4,13 @@
 # Licensed under the GNU GPL version 3.0 or later.  See the file LICENSE for details.
 
 from inkex import errormsg
+from shapely.geometry import Point
+from shapely.ops import nearest_points
 
 from ..commands import OBJECT_COMMANDS, add_commands
 from ..elements import Clone
 from ..i18n import _
+from ..utils.geometry import ensure_multi_polygon
 from .commands import CommandsExtension
 
 
@@ -46,5 +49,32 @@ class ObjectCommands(CommandsExtension):
 
         for element in self.elements:
             if element.node not in seen_nodes and element.shape and not isinstance(element, Clone):
-                add_commands(element, commands)
+                position = self._get_position(element)
+                add_commands(element, commands, pos=position)
                 seen_nodes.add(element.node)
+
+    def _get_position(self, element):
+        command_position = self.options.command_position
+        position = None
+        if command_position != "random":
+            outline, bounds = self._get_element_outline_and_bounds(element)
+
+        if command_position == "top_left":
+            position = nearest_points(outline, Point(bounds[0], bounds[1]))[0]
+        elif command_position == "top_right":
+            position = nearest_points(outline, Point(bounds[2], bounds[1]))[0]
+        elif command_position == "bottom_left":
+            position = nearest_points(outline, Point(bounds[0], bounds[3]))[0]
+        elif command_position == "bottom_right":
+            position = nearest_points(outline, Point(bounds[2], bounds[3]))[0]
+        return position
+
+    def _get_element_outline_and_bounds(self, element):
+        if element.name == "Stroke":
+            shape = element.as_multi_line_string()
+        else:
+            shape = element.shape
+        shape = ensure_multi_polygon(shape.buffer(0.001)).geoms[-1]
+        outline = shape.exterior
+        bounds = outline.bounds
+        return outline, bounds
