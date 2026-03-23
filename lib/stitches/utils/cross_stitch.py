ff5c18a32e486753e9dc3a71b11fb24a79a66c5b
Claudine <88194877+claudinepeyrat06@users.noreply.github.com>
2026-03-23 11:41:52 +0100

    Update cross_stitch.py
    Add some comments

--- lib/stitches/utils/cross_stitch.py
+++ lib/stitches/utils/cross_stitch.py
@@ -291,6 +291,7 @@ class UprightCross(Cross):
 
 
 class DoubleCross(Cross):
+    # Top Cross is the normal cross
     # Double Cross has the same corners and diagonals as normal Cross, so no
     # need to override __init__()
 
@@ -352,13 +353,20 @@ class DoubleCross(Cross):
 
 
 class SmyrnaCross(Cross):
+    # Top cross is the upright cross, this is the usual handstitch order for double crosses.
+    # To avoid bad traveling, we favor connecting the cycles through the top middle points,
+    # and bottom middle points, any other connecting  point creates bad traveling.
+    # We will need to connect via diagonal point when the crosses are joined only at diagonal
+    # We do not use connection via middle left and middle right point as when they exist,
+    # the connection via diagonal is also possible.
+    # As end result, there will be exactly one bad traveled cross on each connected column of Smyrna crosses.
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.good_points = [self.middle_top, self.middle_bottom]
         self.bad_points = [self.top_left, self.top_right, self.bottom_left, self.bottom_right]
         self.all_connection_points = self.good_points + self.bad_points
-    #    self.stitches = (self.good_points, self.bad_points)
 
     def cycle_from_point(self, starting_point):
         if starting_point == self.middle_top:
